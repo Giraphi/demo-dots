b9df0cb2a3fd3fe65b0539a7d05294157fe1b1be
raphaelhoeps <hoeps.raphael@funct.com>
2023-12-05 14:25:31 +0100

    delete comment

--- src/context/MousePositionContextProvider.tsx
+++ src/context/MousePositionContextProvider.tsx
@@ -7,13 +7,9 @@ export interface MousePositionContextProviderProps {
 
 export default function MousePositionContextProvider(props: MousePositionContextProviderProps) {
     const mousePositionRef = useRef<{ x: number; y: number } | undefined>(undefined);
-    // const device = useContext(DeviceContext).device;
 
     useEffect(() => {
         function onMouseMove(e: MouseEvent) {
-            // if (typeof device === "undefined" || device <= Device.sm) {
-            //     return;
-            // }
             mousePositionRef.current = { x: e.pageX, y: e.pageY };
         }
 
